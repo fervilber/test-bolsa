1a03daa8ca78edadb45ba88cda43a7b6c4704131
fervilber <fvillalba@acuamed.es>
2016-10-06 09:52:01 +0200

    cambios

---
+++ leedatos.r
@@ -0,0 +1,66 @@
+Sys.Date()
+# Establece el directorio de trabajo
+setwd("C:/R/ibex/");
+
+#librerias necesarias
+#library("ggplot2", lib.loc="~/R/win-library/3.1");
+library("tseries");
+library("timeSeries");
+library("timeDate");
+library("dygraphs", lib.loc="~/R/win-library/3.1")
+
+#lee tickers de un fichero y lo guarda en un data frame.
+#valoresIbex<-read.table("ibex35.txt",header=TRUE,sep=",")
+valores<-read.table("valores.txt",header=FALSE,sep=",");
+n <-nrow(valores)
+valores;
+
+#funcion para leer los datos de la web de yahoo según el ticker
+yahoo.read <- function(ticker,anos,compresion){
+  #anos<- 1;años o días de datos desde el día de hoy. si no se multiplica por 365 son dias
+  #compresion= puede ser: d=dias w=semanas m=meses
+  acc <- get.hist.quote(instrument= ticker, start= Sys.Date() - anos,end= Sys.Date(), quote="AdjClose",provider="yahoo", origin="1970-01-01", compression=as.character(compresion), retclass="zoo");
+  acc <- acc[!is.na(acc)]; 
+  acc.title = ticker
+  #Calculamos la diferencia entre los valores de hoy y de ayer
+  difacc <- diff(acc);
+  #Calculamos los porcentajes de la diferencia
+  dif100 <-100*difacc/acc;
+  #calculo la serie acumulada
+  acumdif<-cumsum(dif100);
+  return(acumdif)
+}
+
+#------
+anos<-20;  #*365dias
+compre<-as.character("d");
+
+df<- yahoo.read(valores[1,1],anos,compre);
+for (i in 2:n) {
+  acc<- yahoo.read(valores[i,1],anos,compre);
+  names(acc)<-as.character(valores[i,1]);
+  df<-merge(df,acc);
+}
+
+for (i in 1:n) {
+  colnames(df)[i]=as.character(valores[i,1]);
+}
+
+#Uso autoplot() de zoo para pintar las grafias en lugar de ggplot2():
+autoplot(df, facets = NULL);
+
+#uso de dygraph para pintar las graficas
+dygraph(df, main = "evolución desde el Brexit") %>%
+  dyLegend(show = "follow", hideOnMouseOut = TRUE);
+
+#ponemos un listado de los resultados
+x<-df[nrow(df),];
+x=t(x);
+#x[order(x[,1]),];
+y<-x[order(x[,1]),];
+#t(y);
+criterio<- (y>0);
+y[criterio];
+criterio<- (y<0);
+y[criterio];
+autoplot(df, facets = NULL);
--- readme.md
+++ readme.md
@@ -2,13 +2,24 @@
 
 Estos son los cursos incluidos en la especializacion:
 
- * La caja de herramientas del cient√?fico de datos
- * 2R Programming
-* 3Getting and Cleaning Data
-* 4Exploratory Data Analysis
-* 5Reproducible Research
-6Statistical Inference
-7Regression Models
-8Practical Machine Learning
-9Developing Data Products
-10Data Science Capstone
+  * La caja de herramientas del cient√???fico de datos
+  * 2R Programming
+  * 3Getting and Cleaning Data
+  * 4Exploratory Data Analysis
+  * 5Reproducible Research
+  * Statistical Inference
+  * Regression Models
+  * Practical Machine Learning
+  * Developing Data Products
+  * Data Science Capstone
+
+* __In a standard class__
+  * There are 30-100 people
+  * You raise your hand and ask a question
+  * The instructor responds 
+* __In a MOOC__
+  * There are almost 100,000 people
+  * You post a question to the message board
+  * Others vote on your questions
+  * Your instructor responds (as often as possible)
+  * Your peers respond (as often as possible)
